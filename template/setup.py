4367e64a7efde0db086c0d8bc7f39fe0c2bdd705
blakeNaccarato <20692450+blakeNaccarato@users.noreply.github.com>
2020-10-23 21:48:53 -0700

    Rearrange settings, imports, tasks

--- template/setup.py
+++ template/setup.py
@@ -7,17 +7,18 @@ import pathlib
 from setuptools import find_packages, setup
 
 here = pathlib.Path().resolve()
-long_description = (here / 'README.md').read_text(encoding='utf-8')
+long_description = (here / 'README.rst').read_text(encoding='utf-8')
 
 setup(
     name="package_name",
     version="0.0.0",
     description=("Package description."),
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/<user>/<repo>",
-    author="<user>",
+    long_description_content_type="text/x-rst",
+    url="<url>",
+    author="Blake Naccarato",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     python_requires=">=3.7",
+    # install_requires=[],  # For dependencies
 )
